3be39ff5338df9f4d650e4af58cb1ed89b1c7407
raccoonjs <comsamo84@gmail.com>
2018-09-27 02:06:28 +0900

    fb

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,26 @@
 <script>
 export default {
 	name: 'app',
-	components: {}
+	components: {},
+	created () {
+		window.fbAsyncInit = function () {
+			FB.init({
+				appId: '296119561218804',
+				xfbml: true,
+				version: 'v3.1'
+			});
+			FB.AppEvents.logPageView();
+		};
+
+		(function (d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) {return;}
+			js = d.createElement(s); js.id = id;
+			js.src = 'https://connect.facebook.net/ko_KR/sdk.js';
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	}
+
 };
 </script>
 
