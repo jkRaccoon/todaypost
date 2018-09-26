8e3d45619cf2fe342e0957f0eb7316a1227b1ed4
raccoonjs <comsamo84@gmail.com>
2018-09-27 02:15:08 +0900

    fb

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,31 @@
 export default {
 	name: 'app',
 	components: {},
-	created () {
-		window.fbAsyncInit = function () {
-			FB.init({
-				appId: '296119561218804',
-				xfbml: true,
-				version: 'v3.1'
-			});
-			FB.AppEvents.logPageView();
-		};
+	mounted () {
+		this.$nextTick(function() {
+			window.fbAsyncInit = function () {
+				FB.init({
+					appId: '296119561218804',
+					xfbml: true,
+					version: 'v3.1'
+				});
+				FB.AppEvents.logPageView();
+			};
 
-		(function (d, s, id) {
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) {return;}
-			js = d.createElement(s); js.id = id;
-			js.src = 'https://connect.facebook.net/ko_KR/sdk.js';
-			fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
-	}
-
+			(function (d, s, id) {
+				var js;
+				var fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) {
+					return;
+				}
+				js = d.createElement(s);
+				js.id = id;
+				js.src = 'https://connect.facebook.net/ko_KR/sdk.js';
+				fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+		});
+	},
+	created () {}
 };
 </script>
 
